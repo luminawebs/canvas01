75ea7bff21e1646e16d351ff11e9486399c521b4
luminawebs <luminawebs@gmail.com>
2021-12-09 14:37:42 -0500

    Adding files

---
+++ js/generales.js
@@ -0,0 +1,33 @@
+
+		$('#sociosClave').on('click', function () {
+			$('#sociosClaveModal').modal('show');
+	});
+
+	$('.sociosClave').on('click', function () {
+			$('#sociosClaveModal').modal('show');
+	});
+
+	$('.actividadesClave').on('click', function () {
+			$('#actividadesClaveModal').modal('show');
+	});
+	$('.recursosClave').on('click', function () {
+			$('#recursosClaveModal').modal('show');
+	});
+	$('.propuestaValor').on('click', function () {
+			$('#propuestaValorModal').modal('show');
+	});
+	$('.relacionesClientes').on('click', function () {
+			$('#relacionesClientesModal').modal('show');
+	});
+	$('.canales').on('click', function () {
+			$('#canalesModal').modal('show');
+	});
+	$('.segmentosClientes').on('click', function () {
+			$('#segmentosClientesModal').modal('show');
+	});
+	$('.estructurasCostes').on('click', function () {
+			$('#estructurasCostesModal').modal('show');
+	});
+	$('.lineasIngresos').on('click', function () {
+			$('#lineasIngresosModal').modal('show');
+	});
---
+++ js/main.js
@@ -0,0 +1,47 @@
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	var carousel = function() {
+		$('.featured-carousel').owlCarousel({
+	    loop:true,
+	    autoplay: false,
+	    margin:30,
+	    animateOut: 'fadeOut',
+	    animateIn: 'fadeIn',
+	    nav:true,
+	    dots: true,
+	    autoplayHoverPause: false,
+	    items: 1,
+	    navText : ["<p><small>Anterior</small><span class='ion-ios-arrow-round-back'></span></p>","<p><small>Siguiente</small><span class='ion-ios-arrow-round-forward'></span></p>"],
+	    responsive:{
+	      0:{
+	        items:1
+	      },
+	      600:{
+	        items:1
+	      },
+	      1000:{
+	        items:1
+	      }
+	    }
+		});
+
+	};
+	carousel();
+
+})(jQuery);
+
+$('.btn1').click(function(){
+    $('.featured-carousel').trigger('to.owl.carousel', 1);
+});
